575c113cbb671f1c4d6223bb5df52321a3068e89
faizaniq <github email address>
2019-03-06 13:58:46 -0500

    updates

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,24 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    @employee = Employee.where(dog_id: @dog.id)
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.create
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
 end
